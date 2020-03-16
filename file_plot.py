72547c471163594b0a144913aaa7add81ed9f2f1
DundeeB <ligolas@gmail.com>
2020-03-16 22:22:25 +0200

    Changed to normal user input

--- file_plot.py
+++ file_plot.py
@@ -1,53 +1,19 @@
 #!/Local/cmp/anaconda3/bin/python -u
-
 import sys
 import matplotlib.pyplot as plt
+import argparse
 
-if len(sys.argv) < 1:
-    print("Error: too few arguments!")
-    print("Usage: ./plot.py input_data_filename output_filename [plot_title [y_axis_label]]")
-    exit()
+parser = argparse.ArgumentParser(description='plot options')
+parser.add_argument('-f', '--files', type=str, nargs='+', help='files to read data and plot from')
+parser.add_argument('-x', '--x_column', type=int, nargs='*', default=1,  help='x column to plot')
+parser.add_argument('-y', '--y_column', type=int, nargs='*', default=2,  help='y column to plot')
+parser.add_argument('-xL', '--x_label', type=str, nargs='?', default='x', help='x label')
+parser.add_argument('-yL', '--y_label', type=str, nargs='?', default='y', help='y label')
+parser.add_argument('-l', '--legends', type=str, nargs='*', help='legends of plot')
+parser.add_argument('-s', '--style', type=str, nargs='?', default='-', help='legends of plot')
 
-# Read command line arguments
-datafile = sys.argv[1]
-
-outfile = '/dev/null/'
-ylabel = ''
-if len(sys.argv) >= 3:
-    ylabel = sys.argv[2]
-    if len(sys.argv) >=4:
-        outfile = sys.argv[3]
-
-# Read input data file
-labels = []
-data = []
-
-with open(datafile) as f:
-    content = f.read().splitlines()
-    # First row is assumed to contain labels for each column
-    labels = [l.strip() for l in content[0].split(' ')]
-
-    # Use the number of labels to determine the number of data columns
-    data = [[] for _ in range(len(labels))]
-    for line in content[1:]:
-        i = 0
-        for num in line.split():
-            data[i].append(float(num))
-            i += 1
-
-# Initialize plot
-fig, ax = plt.subplots()
-
-# Plot each data set, using first column as x-axis data
-for i in range(1, len(labels)):
-    ax.plot(data[0], data[i], '.', label=labels[i])
-
-# Plot's aesthetics
-ax.set_xlabel(labels[0])
-ax.set_ylabel(ylabel)
-ax.legend()
+args = parser.parse_args()
+for f in args.files:
+    x, y = np.loadtxt(args.files, usecols=(args.x_label, args.y_label), unpack=True)
+    plt.plot(x, y, args.style)
 plt.show()
-# Save figure
-if outfile != '/dev/null/':
-    fig.savefig(outfile)
-    plt.close(fig)
