395fcf5ef0367bf99a9dec88fd8650e030146efd
DundeeB <ligolas@gmail.com>
2020-07-13 18:50:02 +0300

    bugs

--- correlation_ploy.py
+++ correlation_ploy.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import os
+import re
 
 parser = argparse.ArgumentParser(description='plot options')
 parser.add_argument('-f', '--files', type=str, nargs='+', help='files to read data and plot from')
@@ -24,25 +26,26 @@ if len(args.y_column) == 1:
 if len(args.style) == 1:
     args.style = [args.style[0] for _ in range(n_xy)]
 
-m = int(args.psi_mn[0])
-n = int(args.psi_mn[1])
+m = int(args.psis_mn[0])
+n = int(args.psis_mn[1])
 i = 0
 plt.figure()
 for f in args.files:
+    corr_file = lambda s: f + '/OP' + [file for file in os.listdir(f + '/OP') if re.match(s, f)][0]
     for x_col, y_col, s in zip(args.x_column, args.y_column, args.style):
         plt.subplot(211)
-        x, y = np.loadtxt(f + 'OP/psi_' + args.psis_mn + '_corr*', usecols=(x_col - 1, y_col - 1), unpack=True)
+        x, y = np.loadtxt(corr_file('psi_' + args.psis_mn + '_corr*'), usecols=(x_col - 1, y_col - 1), unpack=True)
         lbl = f if args.legends is None else args.legends[i]
         plt.loglog(x, y, s, label=lbl + ', $\psi$_{' + args.psis_mn + '}', linewidth=2, markersize=6)
         if args.upper:
-            x, y = np.loadtxt(f + 'OP/upper_psi_1' + str(m * n) + '_corr*', usecols=(x_col - 1, y_col - 1), unpack=True)
+            x, y = np.loadtxt(corr_file('upper_psi_1' + str(m * n) + '_corr*'), usecols=(x_col - 1, y_col - 1), unpack=True)
             plt.loglog(x, y, s, label=lbl + ', upper layer $\psi$_{1' + str(m * n) + '}', linewidth=2, markersize=6)
 
         plt.subplot(212)
-        x, y = np.loadtxt(f + 'OP/positional_theta=*', usecols=(x_col - 1, y_col - 1), unpack=True)
+        x, y = np.loadtxt(corr_file('positional_theta=*'), usecols=(x_col - 1, y_col - 1), unpack=True)
         plt.loglog(x, y - 1, s, label=lbl + ', g($\Delta$x,0)', linewidth=2, markersize=6)
         if args.upper:
-            x, y = np.loadtxt(f + 'OP/upper_positional_theta=*', usecols=(x_col - 1, y_col - 1), unpack=True)
+            x, y = np.loadtxt(corr_file('upper_positional_theta=*'), usecols=(x_col - 1, y_col - 1), unpack=True)
             plt.loglog(x, y - 1, s, label=lbl + ', upper layer g($\Delta$x,0)', linewidth=2, markersize=6)
 
         i += 1
