2128f1ac9ccd83fe0323507a97631e832b0c2ed9
DundeeB <ligolas@gmail.com>
2020-10-28 18:48:16 +0200

    Now its working

--- zip_env.sh
+++ zip_env.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qsub -V -N zipping -o out/zip.out -e out/zip.err -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q S /srv01/technion/danielab/zip_files.sh
+qsub -V -N zipping -o out/zip.out -e out/zip.err -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q S ./zip_files.sh
