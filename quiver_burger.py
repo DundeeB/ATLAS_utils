09f72d1d90b50f1dfcc0ccdca65e17c0a075e894
DundeeB <ligolas@gmail.com>
2020-11-11 11:35:23 +0200

    z colour

--- quiver_burger.py
+++ quiver_burger.py
@@ -13,6 +13,8 @@ def main():
     parser.add_argument('-f', '--folder', type=str, nargs='?', help='folders to plot simulation result from')
     parser.add_argument('-r', '--real', type=str, nargs='?', default='',
                         help='Realization to plot. Default is last realization')
+    parser.add_argument('-z', '--z_colour', type=bool, nargs='?', const=False, default=True,
+                        help='colour upper and lower spheres with different colours')
 
     # handle defaults and load data
     args = parser.parse_args()
@@ -39,7 +41,13 @@ def main():
     # graphs
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.plot(sp[:, 0], sp[:, 1], '.', markersize=6)
+    if args.z_colour:
+        up = np.where(sp[:, 2] > np.mean(sp[:, 2]))
+        down = np.where(sp[:, 2] <= np.mean(sp[:, 2]))
+        plt.plot(sp[:, 0][up], sp[:, 1][up], '.', markersize=6)
+        plt.plot(sp[:, 0][down], sp[:, 1][down], '.', markersize=6)
+    else:
+        plt.plot(sp[:, 0], sp[:, 1], '.', markersize=6)
     plt.quiver(burg[:, 0], burg[:, 1], burg[:, 2], burg[:, 3], angles='xy', scale_units='xy', scale=1)
     plt.legend('Center\'s xy for realization ' + str(real_num), 'Burger field')
     plt.axis('equal')
