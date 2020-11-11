6513cf905c061a21df6bc83c1ba132a3279a7170
DundeeB <ligolas@gmail.com>
2020-11-11 10:20:00 +0200

    Longer time running

--- zip_env.sh
+++ zip_env.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qsub -V -N zipping -o out/zip.out -e out/zip.err -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q S ./zip_files.sh
+qsub -V -N zipping -o out/zip.out -e out/zip.err -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N ./zip_files.sh
