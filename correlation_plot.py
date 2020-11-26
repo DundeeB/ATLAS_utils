6708eb6283d0e3611c46e9e76526638e6ed23bfc
DundeeB <ligolas@gmail.com>
2020-11-26 11:53:55 +0200

    Adapted code design before adding eikr correlation plot

--- correlation_plot.py
+++ correlation_plot.py
@@ -6,16 +6,22 @@ import os
 import re
 
 
+def get_corr_files(OP_sub_dir):
+    phi_files = [corr_file for corr_file in os.listdir(OP_sub_dir) if re.match('corr.*', corr_file)]
+    phi_reals = [int(re.split('\.', re.split('_', corr_file)[-1])[0]) for corr_file in phi_files]
+    i_m = np.argmax(phi_reals)
+    return phi_files[i_m], phi_reals[i_m], phi_files, phi_reals
+
+
 def main():
     parser = argparse.ArgumentParser(description='plot options')
     parser.add_argument('-f', '--folders', type=str, nargs='+', help='folders to plot simulation result from')
     parser.add_argument('-l', '--labels', type=str, nargs='*', help='legends of plot')
     parser.add_argument('-s', '--style', type=str, nargs='*', default=['.-'], help='style of lines')
-    parser.add_argument('-mn', '--psis_mn', type=str, nargs='?', default='23', help='mn=14,23,16')
+    parser.add_argument('-mn', '--psis_mn', type=str, nargs='?', default=None, help='mn=14,23,16')
     parser.add_argument('-up', '--upper', type=bool, nargs='?', const=True, default=False,
                         help='plot upper correlations')
     parser.add_argument('-nbl', '--only_upper', type=bool, nargs='?', const=True, default=False, help='')
-    parser.add_argument('-p', '--poly', type=str, nargs='?', const=True, default=False, help='add polynomial decay')
     parser.add_argument('-a', '--all', type=str, nargs='?', const=True, default=False,
                         help='plot all files in OP files (In comparison with just the last configuration)')
 
@@ -25,54 +31,46 @@ def main():
     if args.labels is None:
         args.labels = args.folders
 
-    m = int(args.psis_mn[0])
-    n = int(args.psis_mn[1])
-    i = 0
+    op_dirs = []
+    if args.psis_mn is not None:
+        m = int(args.psis_mn[0])
+        n = int(args.psis_mn[1])
+        if not args.only_upper:
+            op_dirs.append('psi_' + args.psis_mn)
+        if args.upper or args.only_upper:
+            op_dirs.append('upper_psi_1' + str(m * n))
 
     size = 15
-    params = {'legend.fontsize': 'large',
-              'figure.figsize': (20, 8),
-              'axes.labelsize': size,
-              'axes.titlesize': size,
-              'xtick.labelsize': size * 0.75,
-              'ytick.labelsize': size * 0.75,
-              'axes.titlepad': 25}
+    params = {'legend.fontsize': 'large', 'figure.figsize': (20, 8), 'axes.labelsize': size, 'axes.titlesize': size,
+              'xtick.labelsize': size * 0.75, 'ytick.labelsize': size * 0.75, 'axes.titlepad': 25}
     plt.rcParams.update(params)
     plt.figure()
-
-    max_y_psi, max_y_pos, x_psi, x_pos = 0, 0, 0, 0
+    relevant_files = []
+    relevant_reals = []
     for f, s, lbl in zip(args.folders, args.style, args.labels):
-        try:
-            pattern = 'corr.*'
-            phi_files = [file for file in os.listdir(f + '/OP/psi_' + args.psis_mn + '/') if
-                         re.match(pattern, file)]
-            phi_reals = [int(re.split('\.', re.split('_', file)[-1])[0]) for file in phi_files]
-            i_m = np.argmax(phi_reals)
-            relevant_files = phi_files if args.all else [phi_files[i_m]]
-            relevant_reals = phi_reals if args.all else [phi_reals[i_m]]
-            for corr_file, real in zip(relevant_files, relevant_reals):
-                lbl_ = lbl + ' ' + str(real) if args.all else lbl
-                try:
-                    x, y = np.loadtxt(f + '/OP/psi_' + args.psis_mn + '/' + corr_file, usecols=(0, 1), unpack=True)
-                except ValueError:
-                    x, y = np.loadtxt(f + '/OP/psi_' + args.psis_mn + '/' + corr_file, usecols=(0, 1), unpack=True,
-                                      dtype=complex)
-                    x, y = np.abs(x), np.abs(y)
-                if not args.only_upper:
-                    plt.loglog(x, y, s, label=lbl + ', $\psi_{' + args.psis_mn + '}$', linewidth=2, markersize=6)
-                if np.nanmax(y) > max_y_psi:
-                    max_y_psi = np.nanmax(y)
-                    x_psi = x[np.nanargmax(y)]
-                if args.upper:
-                    up_fold = 'upper_psi_1' + str(m * n)
-                    x, y = np.loadtxt(f + '/OP/' + up_fold + '/' + corr_file, usecols=(0, 1), unpack=True)
-                    plt.loglog(x, y, s, label=lbl + ', upper layer $\psi_{1' + str(m * n) + '}$', linewidth=2,
-                               markersize=6)
-                i += 1
-        except Exception as err:
-            print(err)
-    if args.poly:
-        plt.loglog(x, max_y_psi * np.power(np.array(x) / x_psi, -1.0 / 4), '--', label='$x^{-1/4}$')
+        for op_dir in op_dirs:
+            try:
+                last_file, last_real, phi_files, phi_reals = get_corr_files(f + '/OP/' + op_dir + '/')
+                if args.all:
+                    for corr_file, real in zip(phi_files, phi_reals):
+                        relevant_files.append(corr_file)
+                        relevant_reals.append(real)
+                else:
+                    relevant_files.append(last_file)
+                    relevant_reals.append(last_real)
+                for corr_file, real in zip(relevant_files, relevant_reals):
+                    lbl_ = lbl + ', ' + op_dir
+                    if not args.all:
+                        lbl_ = lbl_ + ', real ' + str(real)
+                    corr_path = f + '/OP/' + op_dir + '/' + corr_file
+                    try:
+                        x, y = np.loadtxt(corr_path, usecols=(0, 1), unpack=True)
+                    except ValueError:
+                        x, y = np.loadtxt(corr_path, usecols=(0, 1), unpack=True, dtype=complex)
+                        x, y = np.abs(x), np.abs(y)
+                    plt.loglog(x, y, s, label=lbl_, linewidth=2, markersize=6)
+            except Exception as err:
+                print(err)
     plt.grid()
     plt.legend()
     plt.xlabel('$\Delta$r [$\sigma$=2]')
