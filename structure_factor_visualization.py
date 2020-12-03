37d4adb69cc0123cf77f375da3ad47504f6a7be7
DundeeB <ligolas@gmail.com>
2020-12-03 12:52:16 +0200

    trying 3d labels

--- structure_factor_visualization.py
+++ structure_factor_visualization.py
@@ -40,10 +40,11 @@ def main():
         kx, ky, S_values = np.loadtxt(join(op_fold, "vec_" + args.real + ".txt"), unpack=True, usecols=(0, 1, 2))
         # graphs
         ax = axs[sub]
-        line = ax.scatter(kx, ky, S_values, '.')
+        line = ax.scatter(kx, ky, S_values, '.', label=args.folder)
         if sub == 0:
             # ax.set_title(args.folder)
-            line.legend(args.folder)
+            line._facecolors2d = line._facecolors3d
+            line._edgecolors2d = line._edgecolors3d
         ax.set_xlabel('$k_x$')
         ax.set_ylabel('$k_y$')
         ax.set_zlabel(lbl)
