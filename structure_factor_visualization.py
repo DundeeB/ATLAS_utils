cd40b2580176f07f3fa29e4feedf808b2e556cfe
DundeeB <ligolas@gmail.com>
2020-12-03 12:12:41 +0200

    bugs

--- structure_factor_visualization.py
+++ structure_factor_visualization.py
@@ -20,38 +20,38 @@ def main():
     args = parser.parse_args()
     fig = plt.figure()
     for fold in args.folders:
-        op_fold = join(fold, 'OP/Bragg_S')
-        for op_fold, lbl, sub in zip([op_fold, op_fold + 'm'], ['$S_m$', '$S$'], [1, 2]):
-            if args.real == '':
-                args.real, _, _, _ = get_corr_files(op_fold)
-            data_path = join(op_fold, args.real)
-            data = np.loadtxt(data_path)
-            S_values = [d[2] for d in data]
-            kx = [d[0] for d in data]
-            ky = [d[1] for d in data]
+        try:
+            op_fold = join(fold, 'OP/Bragg_S')
+            for op_fold, lbl, sub in zip([op_fold, op_fold + 'm'], ['$S_m$', '$S$'], [1, 2]):
+                if args.real == '':
+                    args.real, _, _, _ = get_corr_files(op_fold)
+                data_path = join(op_fold, args.real)
+                data = np.loadtxt(data_path)
+                S_values = [d[2] for d in data]
+                kx = [d[0] for d in data]
+                ky = [d[1] for d in data]
 
-            # font
-            size = 15
-            params = {'legend.fontsize': 'large',
-                      'figure.figsize': (20, 8),
-                      'axes.labelsize': size,
-                      'axes.titlesize': size,
-                      'xtick.labelsize': size * 0.75,
-                      'ytick.labelsize': size * 0.75,
-                      'axes.titlepad': 25}
-            plt.rcParams.update(params)
+                # font
+                size = 15
+                params = {'legend.fontsize': 'large',
+                          'figure.figsize': (20, 8),
+                          'axes.labelsize': size,
+                          'axes.titlesize': size,
+                          'xtick.labelsize': size * 0.75,
+                          'ytick.labelsize': size * 0.75,
+                          'axes.titlepad': 25}
+                plt.rcParams.update(params)
 
-            # graphs
-            ax = fig.add_subplot(2, 1, sub, projection='3d')
-            ax.scatter(kx, ky, S_values, '.')
-            ax.set_xlabel('$k_x$')
-            ax.set_ylabel('$k_y$')
-            ax.set_zlabel(lbl)
-    plt.subplot(211)
-    if len(args.folders) == 1:
-        plt.title(fold)
-    else:
-        plt.legend(args.folders)
+                # graphs
+                ax = fig.add_subplot(2, 1, sub, projection='3d')
+                ax.scatter(kx, ky, S_values, '.')
+                ax.set_xlabel('$k_x$')
+                ax.set_ylabel('$k_y$')
+                ax.set_zlabel(lbl)
+        except Exception as err:
+            print("\n\nException for " + fold)
+            print(err)
+    plt.legend(args.folders)
     # show
     plt.show()
 
